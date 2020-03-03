99fdadaf722779467b876c798ea13ae379838bf5
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-03-03 21:31:30 +0000

    add final solutions to problem set

--- 4_queries/13_name_of_teachers_and_number_of_assistances.sql
+++ 4_queries/13_name_of_teachers_and_number_of_assistances.sql
@@ -1,7 +1,8 @@
-SELECT DISTINCT teachers.name, cohorts.name
+SELECT teachers.name, cohorts.name, COUNT(assistance_requests.teacher_id) AS total_assistances
   FROM teachers 
   JOIN assistance_requests ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
     WHERE cohorts.name = 'JUL02'
+    GROUP BY teachers.name, cohorts.name
       ORDER BY teachers.name;
